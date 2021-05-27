1b3eab24d6b081ec0153c4bdd03ede76d4bd8943
Austine Gomez <austine.gomez@yahoo.com>
2021-05-26 20:19:25 -0500

    css for footer still needs work for other devices

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -12,10 +12,7 @@ export default class LandingPage extends Component {
         <Signs />
       <div class="d-flex flex-column">
           <footer className="footer">
-            <div>
-              <a href="https://www.austinegomez.com">Zodiaks</a>
-              <span>&copy; 2021 agdigital</span>
-            </div>
+            
             <div className="ml-auto">
               <span>Devloped by</span>
               <a href="https://www.austinegomez.com">Austine Gomez</a>
